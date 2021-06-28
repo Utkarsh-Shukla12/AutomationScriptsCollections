0537bbce78a228d28167cfb4c2881b925dd7d8dd
Utkarsh-Shukla12 <58844190+Utkarsh-Shukla12@users.noreply.github.com>
2021-06-28 18:51:53 +0530

    Update Eventfilter.py
    EventFilter Example

--- Eventfilter.py
+++ Eventfilter.py
@@ -1,12 +1,15 @@
+# Owner: Utkarsh Shukla
+# EventFilter for MXTOWEBPORT_METR_PC
+
 
 from psdi.iface.mic import IntegrationContext
 from psdi.util.logging import MXLoggerFactory	
 logger = MXLoggerFactory.getLogger("maximo.autoscript")
-logger.debug("WAM:::DEBUG:::PUBLISH.WAM_MXTOWEBPORT_METR_PC.EVENTFILTER:::START:::")
+logger.debug("::DEBUG:::PUBLISH.MXTOWEBPORT_METR_PC.EVENTFILTER:::START:::")
 if (ifaceName=="WAM_MXTOWEBPORT_METR_PC"):
 	context = IntegrationContext.getCurrentContext()
 	# to check if the source type is not null
 	if ((context is not None) and (context.getStringProperty("sourcetype") is not None)):
 		evalresult = False
 		
-logger.debug("WAM:::DEBUG:::PUBLISH.WAM_MXTOWEBPORT_METR_PC.EVENTFILTER:::END:::")
+logger.debug("::DEBUG:::PUBLISH.WAM_MXTOWEBPORT_METR_PC.EVENTFILTER:::END:::")
